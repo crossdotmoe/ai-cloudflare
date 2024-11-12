7640e37e5282d97bafb2cfc4102083f271f28c46
cross <crossface360@gmail.com>
2024-11-13 02:24:26 +0530

    Update route.ts

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
 			return new NextResponse("Prompt is required", { status: 400 });
 		}
 		const response = await fetch(
-			`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/black-forest-labs/flux-1-schnell`,
+			`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/runwayml/stable-diffusion-v1-5-img2img`,
 			{
 				method: "POST",
 				headers: {
