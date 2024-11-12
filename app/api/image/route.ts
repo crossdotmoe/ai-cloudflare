480a739ab222af07c1339678addfe2a44cdd3b40
cross <crossface360@gmail.com>
2024-11-13 02:36:22 +0530

    Update route.ts

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
 			return new NextResponse("Prompt is required", { status: 400 });
 		}
 		const response = await fetch(
-			`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/lykon/dreamshaper-8-lc`,
+			`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/lykon/dreamshaper-8-lcm`,
 			{
 				method: "POST",
 				headers: {
